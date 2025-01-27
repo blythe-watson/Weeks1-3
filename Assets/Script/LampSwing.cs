6bec485960128318059d9af92dfc435fddec12db
Blythe Watson <watsonbl@shernet.sheridancollege.ca>
2025-01-27 13:56:09 -0500

    messed around with lerp and other things to get the lamp to swing smoothly, haven't figured it out yet

--- Assets/Script/LampSwing.cs
+++ Assets/Script/LampSwing.cs
@@ -1,9 +1,19 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LampSwing : MonoBehaviour
 {
+
+    public AnimationCurve swing;
+
+    [Range(0,1)]
+    public float t;
+
+    public float direction = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +23,44 @@ public class LampSwing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    //this is a simple function that makes a circle (the axis point) spin
         Vector3 rot = transform.eulerAngles;
 
-        rot.z += 1;
 
+        //t represents where we are on the curve
+        //limit can be either negative one or positive one, meaning that t can go up or down the curve
+        //deltaTime is in there to smooth things out
+        t += direction * Time.deltaTime;
+
+        //every frame, a value is either added or subtracted from the z rotation of the axis point
+        //the value is based on the animation curve to give the illusion of a swinging motion, multiplied by the direction (-1 or 1)
+        //rot.z += Mathf.Lerp((direction * swing.Evaluate(t)), -0.7f, 0.7f);
+        rot.z += direction * swing.Evaluate(t);
+        
+
+        // rot.z += limit;
+
+        //if t reaches the upper end of its range, the direction flips
+        if(t >= 1)
+        {
+            t = 1;
+            direction *= -1;
+            //rot.z = 37;
+        }
+
+        if(t <= 0)
+        {
+            t = 0;
+            direction *= -1;
+            //rot.z = -37;
+        }
+
+        /* if (t >= 1 || t <= 0)
+         {
+             limit = limit * -1;
+         }
+ */
         transform.eulerAngles = rot;
     }
 }
